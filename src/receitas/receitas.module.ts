fdfef0bb0c8fa0abe1f36877ce4f22fafd6be630
Douglas Lucio <dlucio.douglas@gmail.com>
2026-05-19 16:42:38 -0300

    fix: corrige receitas.module.ts

--- src/receitas/receitas.module.ts
+++ src/receitas/receitas.module.ts
@@ -1,30 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DespesasModule } from './despesas/despesas.module';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { IngredientesModule } from './ingredientes/ingredientes.module';
-import { ReceitasModule } from './receitas/receitas.module';
+import { Receita } from './receita.entity';
+import { ReceitasService } from './receitas.service';
+import { ReceitasController } from './receitas.controller';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-    DespesasModule,
-    AuthModule,
-    UsersModule,
-    IngredientesModule,
-    ReceitasModule,
-  ],
+  imports: [TypeOrmModule.forFeature([Receita])],
+  providers: [ReceitasService],
+  controllers: [ReceitasController],
 })
-export class AppModule {}
+export class ReceitasModule {}
