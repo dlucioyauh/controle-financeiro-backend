a1f0eea451db048309d1d041ae45daf359f8abbd
Douglas Lucio <dlucio.douglas@gmail.com>
2026-05-22 14:08:01 -0300

    fix: authguard usando JWT_SECRET do ambiente via ConfigService

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -5,29 +5,30 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 import { Request } from 'express';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
-
     if (!token) {
       throw new UnauthorizedException('Token não encontrado');
     }
-
     try {
       const payload = await this.jwtService.verifyAsync(token, {
-        secret: 'minha_chave_secreta_123',
+        secret: this.configService.get<string>('JWT_SECRET'),
       });
       request['user'] = payload;
     } catch {
       throw new UnauthorizedException('Token inválido');
     }
-
     return true;
   }
 
