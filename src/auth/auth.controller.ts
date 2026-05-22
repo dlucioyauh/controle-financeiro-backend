ce585633ab918689285f287f757c8f9fc1bc2de4
Douglas Lucio <dlucio.douglas@gmail.com>
2026-05-22 15:27:46 -0300

    fix: imports cookieParser e Response para compatibilidade TS

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Post, Res, HttpCode } from '@nestjs/common';
-import { Response } from 'express';
+import type { Response } from 'express';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
