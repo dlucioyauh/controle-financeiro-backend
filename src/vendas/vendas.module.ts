e0aa1936dd197e578d9baff2b1eef23cfb820393
Douglas Lucio <dlucio.douglas@gmail.com>
2026-05-22 14:36:30 -0300

    fix: adiciona AuthModule no VendasModule

--- src/vendas/vendas.module.ts
+++ src/vendas/vendas.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { VendasController } from './vendas.controller';
 import { VendasService } from './vendas.service';
-import { VendaEntity } from './venda.entity'; // Corrigido aqui
+import { VendaEntity } from './venda.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([VendaEntity])], // Corrigido aqui
+  imports: [TypeOrmModule.forFeature([VendaEntity]), AuthModule],
   controllers: [VendasController],
   providers: [VendasService],
   exports: [VendasService],
