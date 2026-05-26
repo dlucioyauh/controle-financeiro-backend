867ffc087db1e57073d07dab05426ff34be7cc51
Douglas Lucio <dlucio.douglas@gmail.com>
2026-05-25 21:18:02 -0300

    main alterado

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://controle-financeiro-frontend-two.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
