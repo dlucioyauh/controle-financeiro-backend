caaa009796a44096b421b7ea06b8329ebdb23a01
dlucioyauh <dlucio.douglas@gmail.com>
2026-06-01 19:22:47 -0300

    fix: adiciona UsersModule no ClientesModule

--- src/clientes/clientes.module.ts
+++ src/clientes/clientes.module.ts
@@ -3,15 +3,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Customer } from './customer.entity';
 import { ClientesService } from './clientes.service';
 import { ClientesController } from './clientes.controller';
+import { LimiteClientesGuard } from './limite-clientes.guard';
 import { AuthModule } from '../auth/auth.module';
+import { UsersModule } from '../users/users.module';   // ← NOVO
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Customer]),
     AuthModule,
+    UsersModule,   // ← NOVO
   ],
   controllers: [ClientesController],
-  providers: [ClientesService],
-  exports: [ClientesService],   // ← ESSENCIAL para que outros módulos possam injetá-lo
+  providers: [ClientesService, LimiteClientesGuard],
+  exports: [ClientesService],
 })
 export class ClientesModule {}
