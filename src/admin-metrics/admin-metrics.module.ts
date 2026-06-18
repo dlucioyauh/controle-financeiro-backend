d804093bf3d21b1366468323949b36ae4ebb1248
dlucioyauh <dlucio.douglas@gmail.com>
2026-06-17 21:39:11 -0300

    fix: import AuthModule in AdminMetricsModule to resolve JwtService dependency

--- src/admin-metrics/admin-metrics.module.ts
+++ src/admin-metrics/admin-metrics.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminMetricsController } from './admin-metrics.controller';
 import { AdminMetricsService } from './admin-metrics.service';
 import { UserEntity } from '../users/user.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+    AuthModule, // ← necessário para o AuthGuard
+  ],
   controllers: [AdminMetricsController],
   providers: [AdminMetricsService],
 })
