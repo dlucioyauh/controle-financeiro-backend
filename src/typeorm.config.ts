b2ffee2dc737ec9dc84b697eb98dd71e1792392b
dlucioyauh <dlucio.douglas@gmail.com>
2026-07-28 21:34:31 -0300

    Corrige configuracao de migracoes

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -13,7 +13,8 @@ export const AppDataSource = new DataSource({
   username: configService.get('DB_USER'),
   password: configService.get('DB_PASS'),
   database: configService.get('DB_NAME'),
-  entities: ['dist/**/*.entity{.ts,.js}'],
+  // 🔥 AGORA APONTA PARA OS ARQUIVOS .TS NA PASTA SRC
+  entities: ['src/**/*.entity{.ts,.js}'],
   migrations: ['src/migrations/*{.ts,.js}'],
   synchronize: false,
 });
