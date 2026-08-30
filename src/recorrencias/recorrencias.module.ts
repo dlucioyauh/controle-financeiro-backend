194f1fea6651c0728294390596ffe2898e9e1326
dlucioyauh <dlucio.douglas@gmail.com>
2026-08-29 23:52:38 -0300

    fix: import AuthModule into RecorrenciasModule to resolve JwtService dependency for AuthGuard

--- src/recorrencias/recorrencias.module.ts
+++ src/recorrencias/recorrencias.module.ts
@@ -4,12 +4,14 @@ import { RecorrenciaEntity } from './recorrencia.entity';
 import { RecorrenciasService } from './recorrencias.service';
 import { RecorrenciasController } from './recorrencias.controller';
 import { RecorrenciasScheduler } from './recorrencias.scheduler';
-import { DespesaEntity } from '../despesas/despesa.entity'; // Ajuste se necessário
+import { DespesaEntity } from '../despesas/despesa.entity';
 import { VendaEntity } from '../vendas/venda.entity';
+import { AuthModule } from '../auth/auth.module'; 
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([RecorrenciaEntity, DespesaEntity, VendaEntity]),
+    AuthModule, 
   ],
   controllers: [RecorrenciasController],
   providers: [RecorrenciasService, RecorrenciasScheduler],
